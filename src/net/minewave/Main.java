50f01748f275139f3eb69ce40bf6ac8cf715fd1a
bradendavis <bradendavis@192.168.0.3>
2017-06-13 20:26:05 -0400

    added onEnable method

--- src/net/minewave/Main.java
+++ src/net/minewave/Main.java
@@ -1,4 +1,13 @@
-public class Main {
+package net.minewave;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	public void onEnable(){
+		
+		//this is the onEnable method.
+	}
 
 
 
